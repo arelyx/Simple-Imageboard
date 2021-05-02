ce5ff01f9b4ef25c73a641bfeac5a052b3664e3b
Harshit Gupta <harshitgupta890@gmail.com>
2021-05-01 20:12:13 -0700

    Can post image

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -9,7 +10,9 @@ db = SQLAlchemy(app)
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    image = db.Column(db.LargeBinary())
+    filename = db.Column(db.Text)
+    mimetype = db.Column(db.Text)
     text = db.Column(db.Text)
     date = db.Column(db.DateTime, default=datetime.now)
 
@@ -17,14 +20,17 @@ class Posts(db.Model):
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        name = request.form.get("name")
+        img = request.files["img"]
+        filename = secure_filename(img.filename)
+        mimetype = img.mimetype
         text = request.form.get("text")
-        new_post = Posts(name=name,text=text)
+        new_post = Posts(image=img.read(),filename=filename,\
+            mimetype=mimetype,text=text)
         db.session.add(new_post)
         db.session.commit()
-        return f"{name} {text}"
+        return redirect("/")
     posts = Posts.query.order_by(Posts.id.desc())
     return render_template("index.html", posts=posts[0:30])
 
 if __name__ == "__main__":
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0")
